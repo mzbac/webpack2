567b89ff8ece91167e934d6b9bcffb9b0c9bff96
anchen <you@example.com>
2017-01-12 22:04:30 +1100

    add css-modules and ExtractTextPlugin.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import './index.css';
+import styles from  './index.css';
 
 const image = document.createElement('img');
 image.src = 'http://lorempixel.com/400/400';
+console.log(styles)
+image.className = styles.img;
 document.body.appendChild(image);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
@@ -13,11 +14,26 @@ const config = {
         test: /\.js$/
       },
       {
-        use: ['style-loader', 'css-loader'],
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: [
+            {
+              loader: 'css-loader',
+              query: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: '[name]__[local]__[hash:base64:5]'
+              }
+            }
+          ]
+        }),
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
 
 module.exports = config;
